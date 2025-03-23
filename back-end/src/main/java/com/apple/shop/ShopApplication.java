a7eb07b71cae5a81edc46f565364019cd144762b
이승환 <seaung13@gmail.com>
2025-03-24 03:51:18 +0900

    chore(build.gradle): 라이브러리 정리

--- back-end/src/main/java/com/apple/shop/ShopApplication.java
+++ back-end/src/main/java/com/apple/shop/ShopApplication.java
@@ -3,7 +3,7 @@ package com.apple.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.apple.shop", "com.exaple.demo"}) // 두 패키지를 스캔하도록 설정
+@SpringBootApplication
 public class ShopApplication {
 
 	public static void main(String[] args) {
